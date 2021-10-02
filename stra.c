9ad3423433d63195963b80ce3e10149829ccdd58
Ellen Su <eysu@cs.princeton.edu>
2021-10-02 19:25:00 -0400

    Initial version

--- stra.c
+++ stra.c
@@ -20,9 +20,11 @@ size_t Str_getLength(const char pcSrc[]){
 }
 /* -----------------------------------------------------------------------------------*/
 
-char[] Str_copy(const char pcSrc[]){
+char Str_copy(const char pcSrc[]){
+    size_t length;
+    int i;
     assert(pcSrc != NULL);
-    size_t length = Str_getLength(pcSrc);
+    length = Str_getLength(pcSrc);
     char dest[length + 1];
     for (i = 0; i < length; i++){
         dest[i] = pcSrc[i];
@@ -32,17 +34,21 @@ char[] Str_copy(const char pcSrc[]){
 }
 /* -----------------------------------------------------------------------------------*/
 
-char[] Str_concat(const char pcSrc1[], const char pcSrc2[]){
+char Str_concat(const char pcSrc1[], const char pcSrc2[]){
+    size_t length1;
+    size_t length2;
+    int i;
+    int j;
     assert(pcSrc1 != NULL);
     assert(pcSrc2 != NULL);
-    size_t length1 = Str_getLength(pcSrc1);
-    size_t length2 = Str_getLength(pcSrc2);
+    length1 = Str_getLength(pcSrc1);
+    length2 = Str_getLength(pcSrc2);
     char dest[length1 + length2 + 1];
 
     for (i = 0; i < length1; i++){
         dest[i] = pcSrc1[i];
     }
-    for (i = 0; i < length2; i++){
+    for (j = 0; j < length2; j++){
         dest[length1 + i] = pcSrc2[i];
     }
     dest[length1 + length2] = '\0';  
@@ -53,22 +59,25 @@ char[] Str_concat(const char pcSrc1[], const char pcSrc2[]){
 size_t Str_compare(const char pcSrc1[], const char pcSrc2[]){
     assert(pcSrc1 != NULL);
     assert(pcSrc2 != NULL);
-    size_t length1 = Str_getLength(pcSrc1);
-    size_t length2 = Str_getLength(pcSrc2);
+    size_t length1; 
+    size_t length2; 
     int val1 = 0;
     int val2 = 0;
-
+    int i;
+    int j;
+    length1 = Str_getLength(pcSrc1);
+    length2 = Str_getLength(pcSrc2);
     for (i = 0; i < length1; i++){
         val1 += pcSrc1[i];
     }
     for (i = 0; i < length2; i++){
         val2 += pcSrc2[i];
     }
-    return val1 - val2;
+    return ((size_t)(val1 - val2));
 }
 /* -----------------------------------------------------------------------------------*/
 
-// size_t Str_search(const char pcSrc1[], const char pcSrc2[]){
+/* size_t Str_search(const char pcSrc1[], const char pcSrc2[]){
 //     assert(pcSrc1 != NULL);
 //     assert(pcSrc2 != NULL);
 //     size_t length1 = Str_getLength(pcSrc1);
@@ -78,7 +87,7 @@ size_t Str_compare(const char pcSrc1[], const char pcSrc2[]){
 //     for (i = 0; i < length2; i++){
         
 //     }
-// }
+*/ 
 /* -----------------------------------------------------------------------------------*/
 
 int main(void){
