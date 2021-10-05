9ee5b9b8c00ec66224982066cea211b35c3c0bb1
Ellen Su <eysu@cs.princeton.edu>
2021-10-05 00:44:52 -0400

    Initial version

--- strp.c
+++ strp.c
@@ -82,11 +82,12 @@ char *Str_search(const char *pcSrc1, const char *pcSrc2){
         else {
             psubstring = (char*)pcSrc1;
             while (*pcSrc1 == *p)
+                pcSrc1++;
+                p++;
                 if (*p == '\0'){
                     return (char*)psubstring;
                 }
-                pcSrc1++;
-                p++;
+
             pcSrc1 = psubstring;
             p = (char*)pcSrc2;
         }
